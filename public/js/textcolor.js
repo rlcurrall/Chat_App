c8273cefb4184c08fba56d361d3d15f1da549cd3
Robb Currall <rlcurrall@gmail.com>
2017-08-05 23:58:46 -0400

    Add dropdown to select text color

---
+++ public/js/textcolor.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+  // on click show/hide menu
+  $("#showColors").on('click', function (e) {
+    e.preventDefault();
+    $("#colors").slideToggle("slow", function () {});
+  });
+
+  // if click away from container, hide colors
+  $(document).on('click', function (e) {
+    if(!$(event.target).closest('#container').length) {
+      if($("#colors").is(":visible")) {
+        $('#colors').slideUp('slow', function () {});
+      }
+    }
+  });
+
+  // if black selected
+  $('#black').on('click', function () {
+    $('#showColors').css("color", "#000000");
+    $('#colors').slideUp('slow', function () {});
+    // set users text color to value
+    var userColor = '#000000';
+  });
+
+  // if blue
+  $('#blue').on('click', function () {
+    $('#showColors').css("color", "#1460aa");
+    $('#colors').slideUp('slow', function () {});
+    // set users text color to value
+    var userColor = '#1460aa';
+  });
+
+  // if red
+  $('#red').on('click', function () {
+    $('#showColors').css("color", "#b50000");
+    $('#colors').slideUp('slow', function () {});
+    // set users text color to value
+    var userColor = '#b50000';
+  });
+
+  // if green
+  $('#green').on('click', function () {
+    $('#showColors').css("color", "#24a159");
+    $('#colors').slideUp('slow', function () {});
+    // set users text color to value
+    var userColor = '#24a159';
+  });
+
+  // if purple
+  $('#purple').on('click', function () {
+    $('#showColors').css("color", "#bf6ee0");
+    $('#colors').slideUp('slow', function () {});
+    // set users text color to value
+    var userColor = '#bf6ee0';
+  });
+
+  // if yellow
+  $('#yellow').on('click', function () {
+    $('#showColors').css("color", "#aa8f00");
+    $('#colors').slideUp('slow', function () {});
+    // set users text color to value
+    var userColor = '#aa8f00';
+  });
+
+  // if purple
+  $('#purple').on('click', function () {
+    $('#showColors').css("color", "9a123b");
+    $('#colors').slideUp('slow', function () {});
+    // set users text color to value
+    var userColor = '#9a123b';
+  });
+
+  // if orange
+  $('#orange').on('click', function () {
+    $('#showColors').css("color", "#d35400");
+    $('#colors').slideUp('slow', function () {});
+    // set users text color to value
+    var userColor = '#d35400';
+  });
+});
