3fd340d78ff7445b139e71b0fdd72162e93325b6
Robb Currall <robert@currall.net>
2020-01-11 19:56:39 -0500

    Add class documentation

--- server/app/services/session.service.js
+++ server/app/services/session.service.js
@@ -1,6 +1,20 @@
 const CookieSessionMiddleware = require('../middleware/session.middleware');
 
+/**
+ * Service class to interact with the express session.
+ *
+ * @class SessionService
+ */
 class SessionService {
+    /**
+     * Get session information from a socket using the cookie session
+     * middleware.
+     *
+     * @static
+     * @param {SocketIO.Socket} socket
+     * @returns {Object}
+     * @memberof SessionService
+     */
     static getSessionFromSocket(socket) {
         let cookieString = socket.request.headers.cookie;
         let session = null;
