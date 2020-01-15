835945628b05c3e02544cb50aae92012f00dcb55
Robb Currall <rlcurra@castlebranch.com>
2020-01-15 15:04:54 -0500

    Add alpinejs, exploring possible uses

--- www/vendor.js
+++ www/vendor.js
@@ -2,3 +2,4 @@ window.$ = require('jquery');
 window.moment = require('moment');
 window.mustache = require('mustache');
 window.io = require('socket.io-client');
+window.Alpinejs = require('alpinejs');
