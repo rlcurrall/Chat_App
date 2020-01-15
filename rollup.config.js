905932073554797d2d2698fe9249c495d7ebc1c8
Robb Currall <rlcurra@castlebranch.com>
2020-01-15 11:34:43 -0500

    Split vendor code in build process

--- rollup.config.js
+++ rollup.config.js
@@ -4,29 +4,51 @@ const cleanup = require('rollup-plugin-cleanup');
 const commonjs = require('@rollup/plugin-commonjs');
 const resolve = require('@rollup/plugin-node-resolve');
 
-module.exports = {
-    input: ['./www/app.js'],
-    plugins: [
-        postcss({
-            extract: true,
-            plugins: [require('autoprefixer'), require('tailwindcss')],
-        }),
-        resolve({
-            preferBuiltins: true,
-            mainFields: ['browser'],
-        }),
-        commonjs(),
-        cleanup(),
-        copy({
-            targets: [{ src: 'www/img/**/*', dest: 'public/img' }],
-        }),
-    ],
-    output: [
-        {
-            file: 'public/app.js',
-            format: 'iife',
-            compact: true,
-            sourcemap: false,
-        },
-    ],
-};
+module.exports = [
+    {
+        input: ['www/vendor.js'],
+        plugins: [
+            resolve({
+                preferBuiltins: true,
+                mainFields: ['browser'],
+            }),
+            commonjs(),
+        ],
+        output: [
+            {
+                file: 'public/vendor.js',
+                format: 'cjs',
+                compact: true,
+                sourcemap: false,
+            },
+        ],
+    },
+    {
+        input: ['./www/app.js'],
+        plugins: [
+            postcss({
+                extract: true,
+                plugins: [require('autoprefixer'), require('tailwindcss')],
+            }),
+            commonjs(),
+            cleanup(),
+            copy({
+                targets: [{ src: 'www/img/**/*', dest: 'public/img' }],
+            }),
+        ],
+        output: [
+            {
+                file: 'public/app.js',
+                format: 'iife',
+                compact: true,
+                sourcemap: false,
+                globals: {
+                    jquery: '$',
+                    moment: 'moment',
+                    mustache: 'mustache',
+                    'socket.io-client': 'io',
+                },
+            },
+        ],
+    },
+];
---
+++ www/vendor.js
@@ -0,0 +1,4 @@
+window.$ = require('jquery');
+window.moment = require('moment');
+window.mustache = require('mustache');
+window.io = require('socket.io-client');
